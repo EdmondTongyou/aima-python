08928132aa88a27fb28e91b921cb2a95c4c39aaf
EdmondTongyou <tongyouedmond@csu.fullerton.edu>
2022-02-25 17:54:08 -0800

    ahahaha

--- main.py
+++
@@ -1,8 +0,0 @@
-from search import* 
-
-if __name__ == '__main__':
-    wgc = WolfGoatCabbage()
-    solution = depth_first_graph_search(wgc).solution()
-    print(solution)
-    solution = breadth_first_graph_search(wgc).solution()
-    print(solution)
